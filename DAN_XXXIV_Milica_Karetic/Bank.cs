cc994a8fbe5c220c68e72934d56c357fd2afb053
MilicaKaretic <milicakaretic1@gmail.com>
2020-06-18 09:00:47 +0200

    Threads created

---
+++ DAN_XXXIV_Milica_Karetic/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXXIV_Milica_Karetic
+{
+    class Bank
+    {
+        public static int maxAmount = 10000;
+
+        Random rnd = new Random();
+
+        public Bank()
+        {
+
+        }
+
+        public int Withdraw(int amount)
+        {
+            return 0;
+        }
+
+        public void DoTransactions()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Withdraw(rnd.Next(100, 10000));
+            }
+        }
+    }
+}
--- DAN_XXXIV_Milica_Karetic/Program.cs
+++ DAN_XXXIV_Milica_Karetic/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace DAN_XXXIV_Milica_Karetic
 {
     class Program
     {
+        /// <summary>
+        /// Valid positive int input
+        /// </summary>
+        /// <returns></returns>
+        public static int ValidPositiveNumber()
+        {
+            string s = Console.ReadLine();
+            int Num;
+            bool b = Int32.TryParse(s, out Num);
+            while (!b || Num < 0)
+            {
+                Console.Write("Invalid input. Try again: ");
+                s = Console.ReadLine();
+                b = Int32.TryParse(s, out Num);
+            }
+            return Num;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter first number");
+            int firstNum = ValidPositiveNumber();
+            Console.WriteLine("Please enter second number");
+            int secondNum = ValidPositiveNumber();
+
+            int sumNum = firstNum + secondNum;
+
+            Thread[] threads = new Thread[sumNum];
+            Bank b = new Bank();
+
+            for (int i = 0; i < sumNum; i++)
+            {
+                Thread t = new Thread(new ThreadStart(b.DoTransactions));
+                threads[i] = t;
+            }
+
+            for (int i = 0; i < sumNum; i++)
+            {
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
         }
     }
 }
